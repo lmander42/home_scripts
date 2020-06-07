242cb7580a8dac255085113a52406191b2517a95
lowdrant <lmanderson42@gmail.com>
2020-06-06 23:58:44 -0400

    Added vol ctl scripts that go to /opt/home_scripts

--- copy-scripts.sh
+++ copy-scripts.sh
@@ -4,22 +4,34 @@
 #
 
 # Check for user bin directory
-if [ ! -e $HOME/bin ]
+if [ ! -e "$HOME/bin" ]
 then
-    mkdir $HOME/bin
+    mkdir "$HOME/bin"
 fi
 
 # Check for appropriate path
-grep -E "export PATH=*\$HOME/bin*" $HOME/.profile
+grep -E "export PATH=*\$HOME/bin*" $HOME/.profile &> /dev/null
 if [ $? -ne 0 ]
 then
     echo "export PATH=\$PATH:\$HOME/bin" >> $HOME/.profile
 fi
-source $HOME/.bashrc
+source "$HOME/.bashrc"
 
-# Copy
+# Copy non-global scripts
 fdir=$(dirname $(realpath $0))
-for i in $fdir/*
+for i in "$fdir/*"
 do
-    cp $i $HOME/bin
+    echo "$i" | grep vol &> /dev/null
+    if [ $? -ne 0 ]
+    then
+        cp "$i" "$HOME/bin"
+    fi
 done
+
+# Global scripts to /opt/home_scripts
+if [ ! -d /opt/home_scripts ]
+then
+    sudo mkdir /opt/home_scripts
+fi
+sudo cp "$fdir/*vol*" /opt/home_scripts/
+sudo chmod +x /opt/home_scripts/*
